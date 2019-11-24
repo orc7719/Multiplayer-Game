689496f992df0be95ed9dda283c8e6f200752ca5
orc7719 <2338928+orc7719@users.noreply.github.com>
2019-11-24 16:45:42 +0000

    Added Projectiles

---
+++ Assets/NetworkPlayerShooting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class NetworkPlayerShooting : NetworkBehaviour
+{
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] Transform projectileSpawn;
+    [SerializeField] float projectileForce;
+
+    [SerializeField] Animator anim;
+
+    private void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            CmdShoot();
+        }
+    }
+
+    [Command]
+    void CmdShoot()
+    {
+        anim.SetTrigger("Throw");
+
+        GameObject projectile = Instantiate(projectilePrefab, projectileSpawn.position, projectileSpawn.rotation);
+        projectile.GetComponent<Rigidbody>().velocity = projectile.transform.forward * projectileForce;
+        NetworkServer.Spawn(projectile);
+    }
+}

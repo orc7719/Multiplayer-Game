83a5c0befe65bce59d7030c59b428933e42aa874
orc7719 <2338928+orc7719@users.noreply.github.com>
2019-11-29 18:45:23 +0000

    Setup Player Models

---
+++ Assets/FollowTarget.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    [SerializeField] Transform targetPos;
+
+    void Update()
+    {
+        transform.position = targetPos.position;
+    }
+}
---
+++ Assets/MeshRendererToggle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshRendererToggle : MonoBehaviour
+{
+    [SerializeField] Renderer[] rends;
+    private void OnEnable()
+    {
+        for (int i = 0; i < rends.Length; i++)
+        {
+            rends[i].shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        }
+    }
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < rends.Length; i++)
+        {
+            rends[i].shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+        }
+    }
+}

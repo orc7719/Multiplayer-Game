529049aed6606840086a7bbf0943cf23edb5061e
Cory McAdam <orc7719@gmail.com>
2019-11-29 14:46:51 +0000

    Everything Just Broke

---
+++ Assets/LookAtTarget.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+    [SerializeField]
+    Transform lookTarget;
+
+    void Update()
+    {
+        transform.LookAt(lookTarget);
+    }
+}
